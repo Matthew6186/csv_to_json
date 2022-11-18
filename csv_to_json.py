bfe8af849758c9d3b01b4a525548c9d7ee09576e
Matthew6186 <44461592+Matthew6186@users.noreply.github.com>
2022-11-18 17:59:15 +0900

    change sample csv formula

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,26 +1,75 @@
 import os.path
 import csv
 import json
-import pprint
 
 FILENAME = './sample.csv'
 
 def main():
     # csv2tsv_temperature()
-    csv2tsv_vital()
+    # csv2tsv_vital()
+    csv2json_air()
 
 def add_devid(head, tmparr):
     tmpstr = ""
     for i, ihead in enumerate(head):
         # tmpstr += "'" + str(ihead) + "':'" + str(tmparr[i]) + "',"
-        tmpstr += '"' + str(ihead) + '":"' + str(tmparr[i]) + '",'
+        # tmpstr += '"' + str(ihead) + '":"' + str(tmparr[i]) + '",'
+        tmpstr += str(ihead) + ':' + str(tmparr[i]) + ','
         # tmpstr += str(ihead) + ':"' + str(tmparr[i]) + '",'
     tmpstr = tmpstr[:-1]
 
     return tmpstr
 
-def csv2json():
-    pass
+def csv2json_air():
+    outarr = []
+    strarr = []
+    with open(FILENAME,"r") as f:
+        reader = csv.reader(f, quoting=csv.QUOTE_NONE)
+        head = []
+        for i, row in enumerate(reader):
+            tmparr = []
+            tmpjson = []
+            tmpstr = ""
+            tmpdevidstr = ""
+            tmparr.append(row[0])
+            tmparr.append(row[1])
+            tmparr.append(row[2])
+            tmparr.append(row[3])
+            tmparr.append(row[4:])
+            outarr.append(tmparr)
+
+            tmpjson = row[4:]
+            for item in tmpjson:
+                tmpstr += str(item[:]) + ","
+            # tmpstr = tmpstr[:-1]
+            tmpstr = tmpstr[1:-2]
+            if i == 0:
+                # head = [row[0], row[1], row[2], row[3], row[4]]
+                head = [row[0], row[1], row[2], row[3]]
+                # head = [row[0][1:-1], row[1][1:-1], row[2][1:-1], row[3][1:-1]]
+                # head = row
+                strarr.append(head)
+            else:
+                tmpdevidstr = add_devid(head, tmparr)
+                # tmpdevidstr = "{" + tmpdevidstr + "}"
+                # tmpjson0 = json.loads(tmpdevidstr)
+                tmpstr = "{" + tmpdevidstr + ',"data_json":' + tmpstr + "}" 
+                tmpjson = json.loads(tmpstr)
+                strarr.append(json.loads(tmpstr))
+
+    print(strarr[0])
+    print(strarr[1:3])
+
+    with open('./out.tsv', 'w', newline="") as f:
+        writer = csv.writer(f, delimiter='\t', quoting=csv.QUOTE_NONE, escapechar='\\')
+        writer.writerows(outarr)
+
+    prop_name = os.path.basename(FILENAME)
+    outjson = {}
+    outjson[prop_name] = strarr[1:]
+
+    with open('./outdata.json','w') as f:
+        json.dump(outjson, f, indent=4)
 
 def csv2tsv_vital():
     outarr = []
